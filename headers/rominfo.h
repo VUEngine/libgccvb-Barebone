5637f8ce2474a93bd6ea1a17e481240843e435bc
Christian Radke <c.radke@posteo.de>
2023-08-07 20:12:35 +0200

    Fix rominfo.h

--- headers/rominfo.h
+++ headers/rominfo.h
@@ -3,13 +3,13 @@
 
 
 // game title (20 chars) 	 ####################
-#define __GAME_TITLE		"libgccvb Barebone   "
+#define GAME_TITLE			"libgccvb Barebone   "
 // maker code (2 chars)		 ##
-#define __MAKER_CODE		"VB"
+#define MAKER_CODE			"VB"
 // game code (4 chars)		 ####
-#define __GAME_CODE			"VLBM"
+#define GAME_CODE			"VLBM"
 // revision (1.x)			 #
-#define __ROM_VERSION		 0
+#define ROM_VERSION			 0
 
 
 #endif
